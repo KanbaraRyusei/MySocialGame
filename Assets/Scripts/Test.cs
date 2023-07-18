c886439105d6aa0cad8c6f19f204dbcda34b3c54
Ryusei1115 <tekunikaru22@gmail.com>
2023-07-18 13:35:10 +0900

    add unitask and nuget and polly

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using PlayFab;
 using PlayFab.ClientModels;
+using Cysharp.Threading.Tasks;
 
 public class Test : MonoBehaviour
 {
     async void Start()
+    {
+        await CustomIDLogin();
+    }
+
+    private async UniTask CustomIDLogin()
     {
         PlayFabSettings.staticSettings.TitleId = "1936E";
         var request = new LoginWithCustomIDRequest
@@ -15,7 +21,8 @@ public class Test : MonoBehaviour
             CreateAccount = true
         };
         var result = await PlayFabClientAPI.LoginWithCustomIDAsync(request);
-        var message = result.Error is null ? $"Login success! PlayFabID:{result.Result.PlayFabId}" : result.Error.GenerateErrorReport();
-        Debug.Log(message);
+        var log = result.Error is null ? result.Result.PlayFabId
+            : result.Error.ErrorMessage;
+        Debug.Log(log);
     }
 }
