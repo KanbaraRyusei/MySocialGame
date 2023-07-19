6e79bc65a5f030832f1052d83aee75704e6dcb50
Ryusei1115 <tekunikaru22@gmail.com>
2023-07-19 21:00:21 +0900

    add playfablogin

---
+++ Assets/Scripts/PlayfabLogin.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+using Cysharp.Threading.Tasks;
+
+public class PlayfabLogin : MonoBehaviour
+{
+    public async UniTask GetUserData()
+    {
+        var request = new GetUserDataRequest();
+        var result = await PlayFabClientAPI.GetUserDataAsync(request);
+        var log = result.Error is null ? result.Result.Data["Name"].Value
+            : result.Error.GenerateErrorReport();
+        Debug.Log(log);
+    }
+
+    public async UniTask UpdateUserData()
+    {
+        var request = new UpdateUserDataRequest()
+        {
+            Data = new Dictionary<string, string>
+            {
+                { "Name", "Change" }
+            }
+        };
+
+        var result = await PlayFabClientAPI.UpdateUserDataAsync(request);
+
+        if(result.Error is null)
+        {
+            await GetUserData();
+        }
+    }
+}
