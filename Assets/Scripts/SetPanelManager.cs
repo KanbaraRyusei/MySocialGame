450dc6642da98aa30ecbedda48018dd4d7c879bb
Ryusei1115 <tekunikaru22@gmail.com>
2023-11-04 23:11:55 +0900

    add setpanelmanager

---
+++ Assets/Scripts/SetPanelManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetPanelManager : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] _panels;
+
+
+}
